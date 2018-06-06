48a371cddd1355fce40b013f50e7a7bdd5398b19
thomasschoeftner <thomas.schoeftner@gmail.com>
2018-06-06 07:57:25 +0200

    added asseration against slice of errors

--- test/assert.go
+++ test/assert.go
@@ -7,3 +7,12 @@ func CheckError(t *testing.T, e error) {
 		t.Error(e)
 	}
 }
+
+func CheckErrors(t *testing.T, errs []error) {
+	if len(errs) > 0 {
+		t.Error("errors:")
+		for _, e := range errs {
+			t.Errorf("  %v\n", e)
+		}
+	}
+}
