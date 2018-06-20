983b14f9351fd0f2ad76c8d6f9d0d9bf380f997f
thomasschoeftner <thomas.schoeftner@gmail.com>
2018-06-20 14:45:55 +0200

    make type Assertion public

--- test/assert.go
+++ test/assert.go
@@ -24,80 +24,80 @@ func ExpectError(t *testing.T, err error, desc string) {
 }
 
 
-type asserter struct {
-	t *testing.T
+type Assertion struct {
+	T *testing.T
 }
 
-func AssertOn(t *testing.T) *asserter {
-	return &asserter{t}
+func AssertOn(t *testing.T) *Assertion {
+	return &Assertion{t}
 }
 
-func (a *asserter) NotError(e error) {
+func (a *Assertion) NotError(e error) {
 	if e != nil {
-		a.t.Error(e)
-		a.t.FailNow()
+		a.T.Error(e)
+		a.T.FailNow()
 	}
 }
 
-func (a* asserter) ExpectError(msg string) func (error) {
+func (a *Assertion) ExpectError(msg string) func (error) {
 	return func(e error) {
 		if e == nil {
-			a.t.Error(msg)
-			a.t.FailNow()
+			a.T.Error(msg)
+			a.T.FailNow()
 		}
 	}
 }
 
-func (a *asserter) StringNotError(s string, e error) string {
+func (a *Assertion) StringNotError(s string, e error) string {
 	if e != nil {
-		a.t.Error(e)
-		a.t.FailNow()
+		a.T.Error(e)
+		a.T.FailNow()
 		return "_error_" //not relevant as FailNow will cut execution anyway
 	}
 	return s
 }
 
-func (a *asserter) BoolNotError(b bool, e error) bool {
+func (a *Assertion) BoolNotError(b bool, e error) bool {
 	if e != nil {
-		a.t.Error(e)
-		a.t.FailNow()
+		a.T.Error(e)
+		a.T.FailNow()
 		return false //not relevant as FailNow will cut execution anyway
 	}
 	return b
 }
 
-func (a *asserter) Is(expected bool, msg string) func(bool) {
+func (a *Assertion) Is(expected bool, msg string) func(bool) {
 	return func(condition bool) {
 		if expected != condition {
-			a.t.Error(msg)
-			a.t.FailNow()
+			a.T.Error(msg)
+			a.T.FailNow()
 		}
 	}
 }
 
-func (a *asserter) True(msg string) func(bool) {
+func (a *Assertion) True(msg string) func(bool) {
 	return a.Is(true, msg)
 }
 
-func (a *asserter) False(msg string) func(bool) {
+func (a *Assertion) False(msg string) func(bool) {
 	return a.Is(false, msg)
 }
 
-func (a *asserter) TrueNotError(msg string) func(bool, error) {
+func (a *Assertion) TrueNotError(msg string) func(bool, error) {
 	return func(b bool, e error) {
 		a.True(msg)(a.BoolNotError(b, e))
 	}
 }
 
-func (a *asserter) FalseNotError(msg string) func(bool, error) {
+func (a *Assertion) FalseNotError(msg string) func(bool, error) {
 	return func(b bool, e error) {
 		a.False(msg)(a.BoolNotError(b, e))
 	}
 }
 
-func (a *asserter) StringsEqual(expected, got string) {
+func (a *Assertion) StringsEqual(expected, got string) {
 	if expected != got {
-		a.t.Errorf("string mismatch - expected %s, but got %s", expected, got)
-		a.t.FailNow()
+		a.T.Errorf("string mismatch - expected %s, but got %s", expected, got)
+		a.T.FailNow()
 	}
 }
